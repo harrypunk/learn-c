1bf5f89fca789becf8cbb78446ce5301c6d83ea3
hamo <allthatjazz@outlook.com>
2020-04-03 10:28:42 -0400

    int and float

---
+++ app.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+
+int main(){
+  int a,b;
+  int c;
+  float f;
+
+  a=10;
+  b=20;
+  c=a+b;
+  f=70.0/3.0;
+  printf("value f :%f \n",f);
+  printf("value c :%d \n",c);
+
+  return 0;
+}
