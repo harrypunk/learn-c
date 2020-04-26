3c9ee82f3ba349377974c9856d0bfcba91c5af9d
hamo <allthatjazz@outlook.com>
2020-04-26 10:23:35 -0400

    array

--- app.c
+++ app.c
@@ -1,16 +1,12 @@
-#include <float.h>
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-  int a;
-  int *d;
+  int arr[5] = {1, 2, 3, 11, 12};
 
-  a = 10;
-
-  d = &a;
-  printf("value d :%d \n", *d);
+  for (int i = 0; i <= 4; ++i) {
+    printf("%d\n", arr[i]);
+  }
 
   return 0;
 }
